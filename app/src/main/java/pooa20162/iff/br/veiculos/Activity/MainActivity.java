03df92fa03d0c95326f17ff54e1ec752de581a43
Tdust <gomes.tdust@gmail.com>
2017-03-06 19:33:58 -0300

    Criacao da estrutura

--- app/src/main/java/pooa20162/iff/br/veiculos/Activity/MainActivity.java
+++ app/src/main/java/pooa20162/iff/br/veiculos/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package pooa20162.iff.br.veiculos;
+package pooa20162.iff.br.veiculos.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import pooa20162.iff.br.veiculos.R;
 
 public class MainActivity extends AppCompatActivity {
 
