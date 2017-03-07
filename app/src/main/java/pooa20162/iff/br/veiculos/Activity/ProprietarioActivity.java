dff461bdc548b2f3854f9203b9b1699f96d8a162
Tdust <gomes.tdust@gmail.com>
2017-03-06 21:49:06 -0300

    Possiveis solucoes

--- app/src/main/java/pooa20162/iff/br/veiculos/Activity/ProprietarioActivity.java
+++ app/src/main/java/pooa20162/iff/br/veiculos/Activity/ProprietarioActivity.java
@@ -1,15 +1,43 @@
 package pooa20162.iff.br.veiculos.Activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import pooa20162.iff.br.veiculos.Model.Proprietario;
 import pooa20162.iff.br.veiculos.R;
 
 public class ProprietarioActivity extends Activity {
+    String nome, endereco, telefone, data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proprietario);
+
+        Button btCriar = (Button) findViewById(R.id.btCriar);
+
+        btCriar.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                salvar();
+                Intent intent = new Intent(ProprietarioActivity.this, ListaProprietarioActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void salvar() {
+        EditText etnome = (EditText) findViewById(R.id.etNome);
+        EditText etendereco = (EditText) findViewById(R.id.etEndereco);
+        EditText ettelefone = (EditText) findViewById(R.id.etTelefone);
+        EditText etdata = (EditText) findViewById(R.id.etData);
+
+        Proprietario pessoa = new Proprietario(etnome.getText().toString(), etendereco.getText().toString(), ettelefone.getText().toString(), etdata.getText().toString());
+        pessoa.save();
     }
 }
---
+++ app/src/main/java/pooa20162/iff/br/veiculos/Adapter/ProprietarioAdapter.java
@@ -0,0 +1,45 @@
+package pooa20162.iff.br.veiculos.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import pooa20162.iff.br.veiculos.Model.Proprietario;
+import pooa20162.iff.br.veiculos.R;
+
+/**
+ * Created by aluno on 06/03/17.
+ */
+
+public class ProprietarioAdapter extends ArrayAdapter<Proprietario> {
+    private final Context context;
+    private final ArrayList<Proprietario> elementos;
+
+    public ProprietarioAdapter(Context context, ArrayList<Proprietario> elementos) {
+        super(context, R.layout.linhaProprietario, elementos);
+        this.context = context;
+        this.elementos = elementos;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.linhaProprietario, parent, false);
+        TextView nome = (TextView) rowView.findViewById(R.id.pNome);
+        TextView endereco = (TextView) rowView.findViewById(R.id.pEndereco);
+        TextView telefone = (TextView) rowView.findViewById(R.id.pTelefone);
+        TextView data = (TextView) rowView.findViewById(R.id.pData);
+        nome.setText(elementos.get(position).getNome());
+        endereco.setText(elementos.get(position).getEndereco());
+        telefone.setText(elementos.get(position).getTelefone());
+        data.setText(elementos.get(position).getData());
+        return rowView;
+    }
+}
